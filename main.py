c7f2ec912c3d1d6144fe6cf26c4ddaf0fbbed011
ghdwpaks <hjm7904@gmail.com>
2021-07-25 23:53:44 +0900

    structured codes and met error 'pros_network_control txt file cant read from python'

--- main.py
+++ main.py
@@ -2,67 +2,90 @@ import csv
 import random as r
 from typing import Counter
 
+
 def get_problem_table(filepath) :
+    print("get_problem_table filepath :",filepath)  
     table = []
     with open(filepath,'rt',encoding="UTF8") as f :
         reader = csv.DictReader(f)
+        print("get_problem_table reader :",reader)
         for row in reader :
+            print("get_problem_table row :",row)
             table.append(row)
     return table
 
-def test(pro_table) :
-    right_ans = []
-    for i in range(len(pro_table)) :
-        right_ans.append(pro_table[i]["1"])
-    #print("test right_ans =",right_ans)
-    print(pro_table[0]["2"])
-    
-    ans = get_ans()
-    if check_ans(ans,right_ans) :
-        print("맞았습니다.")
-    else :
-        print("틀렸습니다.")
-        print()
-        print("정답 :",end="")
-        for i in range(len(right_ans)) :
-            print(right_ans[i])
-        print()
-
-
-def check_ans(userans, rightans) :
-    i = 0 
-    right_count = 0
-    right_nums = []
-    while i < len(userans) and i < len(rightans) :
-        if userans[i] in rightans :
-            right_count += 1
-            
-        i += 1
-    if right_count == len(rightans) :
-        return True
-    else :
-        return False
-
-def get_ans() :
-    a = ""
-    res = []
+def get_problem_table_text(text_filepath) :
+    table = []
+    text_data = open(text_filepath,"r")
     while True :
-        a = input("입력 : ")
-        if a == "ㄴ" or a == "s" :
-            break
-        res.append(a)
-    return res
-startnum = 8 #i = 0의 0상태에서 추가할 숫자
+        line = text_data.readline()
+        if not line : break
+        table.append(line)
+    return table
+
+class gen2 :
+    
+    def test_main(pro_table) :
+        print(pro_table)
+
+    def check_ans() :
+        pass
+
+class gen1 :
+    def test_main(pro_table) :
+        right_ans = []
+        for i in range(len(pro_table)) :
+            right_ans.append(pro_table[i]["1"])
+        #print("test right_ans =",right_ans)
+        print(pro_table[0]["2"])
+        
+        ans = gen1.get_ans()
+        if gen1.check_ans(ans,right_ans) :
+            print("맞았습니다.")
+        else :
+            print("틀렸습니다.")
+            print()
+            print("정답 :",end="")
+            for i in range(len(right_ans)) :
+                print(right_ans[i])
+            print()
+
+
+    def check_ans(userans, rightans) :
+        i = 0 
+        right_count = 0
+        right_nums = []
+        while i < len(userans) and i < len(rightans) :
+            if userans[i] in rightans :
+                right_count += 1
+                
+            i += 1
+        if right_count == len(rightans) :
+            return True
+        else :
+            return False
+
+    def get_ans() :
+        a = ""
+        res = []
+        while True :
+            a = input("입력 : ")
+            if a == "ㄴ" or a == "s" :
+                break
+            res.append(a)
+        return res
+startnum = 0 #i = 0의 0상태에서 추가할 숫자
 i = startnum
-den_num = []
+ben_num = []
+gen = 2
 while True :
     #i = r.shuffle[2,4,10,17,35,41]
     '''
     while True :
         choice_list = [2,10,17,35,41,51]
         i = r.choice(choice_list)
-        if len(choice_list) == len(den_num) :
-            den_num.clear()
+        if len(choice_list) == len(ben_num) :
+            ben_num.clear()
 
         if i in den_num :
             continue
@@ -70,24 +93,33 @@ while True :
             break
     '''
             
-    i = r.randint(1,10)
+    #i = r.randint(0,10)
     #i += 1
-    #i = 35
+    i = 1
     ProsFileNum = str(i)
     
-    #network
-    filepath = "./pros_network/"+ProsFileNum+".txt"
+    #network nomal
+    filepath = "./pros_network_nomal/"+ProsFileNum+".txt"
 
     #comsys
     filepath = "./pros_comsys/"+ProsFileNum+""
 
+    #network control
+    filepath = "./pros_network_control/"+ProsFileNum+".txt"
+    print("filepath :",filepath)
     try :
         pro_table = get_problem_table(filepath)
+        print("pro_table :",pro_table)
+        if gen == 1 :
+            gen1.test_main(pro_table)
+        elif gen == 2:
+            print("gen2 도달")
+            gen2.test_main(pro_table)
+        ben_num.append(i)
     except FileNotFoundError :
         i = startnum
         continue
-    test(pro_table)
-    den_num.append(i)
+
 
     print()
 
