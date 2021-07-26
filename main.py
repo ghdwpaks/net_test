dbab7a97c9b52f9152dbb6e479d6e7118aaa2737
ghdwpaks <hjm7904@gmail.com>
2021-07-26 15:25:06 +0900

    made function gen2 sentence test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import csv
 import random as r
 from typing import Counter
+import os
 
 
 def get_problem_table(filepath) :
-    print("get_problem_table filepath :",filepath)  
+    #print("get_problem_table filepath :",filepath)  
     table = []
     with open(filepath,'rt',encoding="UTF8") as f :
         reader = csv.DictReader(f)
-        print("get_problem_table reader :",reader)
+        #print("get_problem_table reader :",reader)
         for row in reader :
-            print("get_problem_table row :",row)
+            #print("get_problem_table row :",row)
             table.append(row)
     return table
 
@@ -23,12 +24,65 @@ def get_problem_table_text(text_filepath) :
         table.append(line)
     return table
 
+def get_ans() :
+    a = ""
+    res = []
+    while True :
+        a = input("입력 : ")
+        if a == "ㄴ" or a == "s" :
+            break
+        res.append(a)
+    return res
+
 class gen2 :
     
     def test_main(pro_table) :
-        print(pro_table)
+        #print(pro_table)
+        pro_table = gen2.setting_pro(pro_table)
+        print(pro_table[0]["show_pro"])
+        userans = get_ans()
+        res = False
+        if pro_table[0]['test_type'] == 's' :
+            res = gen2.check_ans_sentence(userans,pro_table)
+        elif pro_table[0]['test_type'] == 'w' :
+            res = gen2.check_ans_word(userans,pro_table)
+        
+        if res :
+            print("맞았습니다.")
+        else :
+            print("틀렸습니다.")
+            print()
+            print("정답 :",end="")
+            for i in range(len(pro_table)) :
+                print(pro_table[i]['pro_rightans'])
+            print()
 
-    def check_ans() :
+    def setting_pro(pro_table) :
+        if pro_table[0]["test_type"] == 's' :
+            pro_table[0]["pro_rightans"] = str(pro_table[0]["pro_rightans"]).split(".")
+        return pro_table
+    def check_ans_word(userans,pro_table) :
+        if userans == pro_table :
+            return True
+        return False
+    def check_ans_sentence(userans,pro_table) :
+        right_ans_count = 0
+        #print("gen2 check_ans_sentence userans :",userans[0])
+        #print("gen2 check_ans_sentence pro_table :",pro_table)
+        for i in range(len(pro_table)) :
+            temp_right_ans_count = 0
+            for j in range(len(pro_table[i]['pro_rightans'])) :  
+                #print("pro_table[i]['pro_rightans'][j] :",pro_table[i]['pro_rightans'][j])   
+                #print("pro_table[i]['pro_rightans'][j] in userans :",pro_table[i]['pro_rightans'][j] in userans[i])
+                if pro_table[i]['pro_rightans'][j] in userans[i] :
+                    temp_right_ans_count += 1    
+            if temp_right_ans_count == len(pro_table[i]['pro_rightans']) :
+                right_ans_count += 1
+        #print("right_ans_count :",right_ans_count)
+        #print("len(pro_table) :",len(pro_table))
+        if right_ans_count == len(pro_table) :
+            return True
+        return False
         pass
 
 class gen1 :
@@ -39,7 +93,7 @@ class gen1 :
         #print("test right_ans =",right_ans)
         print(pro_table[0]["2"])
         
-        ans = gen1.get_ans()
+        ans = get_ans()
         if gen1.check_ans(ans,right_ans) :
             print("맞았습니다.")
         else :
@@ -65,15 +119,6 @@ class gen1 :
         else :
             return False
 
-    def get_ans() :
-        a = ""
-        res = []
-        while True :
-            a = input("입력 : ")
-            if a == "ㄴ" or a == "s" :
-                break
-            res.append(a)
-        return res
 startnum = 0 #i = 0의 0상태에서 추가할 숫자
 i = startnum
 ben_num = []
@@ -106,20 +151,20 @@ while True :
 
     #network control
     filepath = "./pros_network_control/"+ProsFileNum+".txt"
-    print("filepath :",filepath)
+    #print("filepath :",filepath)
     try :
         pro_table = get_problem_table(filepath)
-        print("pro_table :",pro_table)
+        #print("pro_table :",pro_table)
         if gen == 1 :
             gen1.test_main(pro_table)
         elif gen == 2:
-            print("gen2 도달")
+            #print("gen2 도달")
             gen2.test_main(pro_table)
         ben_num.append(i)
     except FileNotFoundError :
         i = startnum
         continue
-
+    #os.system("pause")
 
     print()
 
